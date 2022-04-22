8c7805fd292a83446636b2850c70126377ae99f2
William Diaz <williamefrain1994@gmail.com>
2022-04-22 17:12:30 -0600

    Proyecto

---
+++ ClasePA/ClasePA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClasePA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
